5bac2d54edb384ddbae5f6ff99fc51998fe9b2cf
Ömer Faruk Özler <95633424+omerfarukozler@users.noreply.github.com>
2025-02-05 17:04:24 +0300

    some changes

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -9,28 +9,12 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Ogrenci> Students { get; set; }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
-        {
-            optionsBuilder.UseMySql("server=loopback.host;port=9900;database=omer;user=omer;pwd=galatasaray.omer;SslMode=none;AllowPublicKeyRetrieval=True;",
-                new MySqlServerVersion(new Version(8, 0, 33)),
-                options =>
-                {
-                    options.EnableRetryOnFailure(
-                        maxRetryCount: 5,
-                        maxRetryDelay: TimeSpan.FromSeconds(10),
-                        errorNumbersToAdd: null);
-                });
-        }
-    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<Ogrenci>(entity =>
         {
             entity.ToTable("Students");
         });
     }
-
 }
--- obj/Debug/net8.0/SimpleWebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimpleWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0878582e6dea1621b91780c3bbcd7e4b92a457a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b108028f8f4d27907898a0662747f150e6a1dc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
